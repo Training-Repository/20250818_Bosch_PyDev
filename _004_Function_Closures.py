7227ebe62d2ed839f4e91a99d5ae027aa8fb0f7b
ramakantsdebata <ramakant.s.debata@gmail.com>
2025-08-20 19:44:10 +0530

    Day 03

--- _004_Function_Closures.py
+++ _004_Function_Closures.py
@@ -1,26 +1,47 @@
-s1 = "Global String"
+# s1 = "Global String"
 
-def Outer(num):
+# def Outer(num):
 
-    def Inner():
-        s1 = "Inner String"
-        print(f"Inner --> {s1}, {num}")
+#     def Inner():
+#         s1 = "Inner String"
+#         print(f"Inner --> {s1}, {num}")
 
-    # Inner()
+#     # Inner()
 
-    s1 = "Outer String"
-    print(f"Outer --> {s1}")
-    return Inner
+#     s1 = "Outer String"
+#     print(f"Outer --> {s1}")
+#     return Inner
 
-fn1 = Outer(10)
-fn2 = Outer(20)
-fn3 = Outer(30)
-
-
-fn1()
-fn2()
-fn3()
-
-print(f"Global --> {s1}")
+# fn1 = Outer(10)
+# fn2 = Outer(20)
+# fn3 = Outer(30)
 
 
+# fn1()
+# fn2()
+# fn3()
+
+# print(f"Global --> {s1}")
+
+###############################################
+
+def PowerOf(exp):
+    def calc(num):
+        return num**exp
+    return calc
+
+p0 = PowerOf(0)
+p1 = PowerOf(1)
+p2 = PowerOf(2)
+p3 = PowerOf(3)
+
+print(p0(750))
+print(p1(750))
+print(p2(2))
+print(p3(2))
+
+lst = []
+for i in range(5):
+    lst.append(PowerOf(i))
+
+print(lst[3](2))
